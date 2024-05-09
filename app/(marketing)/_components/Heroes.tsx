f45e42f8dd658127db146283ea87a35d9c96cc1b
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-05-10 02:10:21 +0430

    set up clerk and convex as well as a rudemintary sidebar

--- app/(marketing)/_components/Heroes.tsx
+++ app/(marketing)/_components/Heroes.tsx
@@ -4,14 +4,31 @@ export const Heroes = () => {
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[400px] md:w-[400px]">
-          <Image src="/bg.png" alt="Document" fill className="object-contain" />
+          <Image
+            src="/bg.png"
+            alt="Document"
+            fill
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/bg-dark.png"
+            alt="Document"
+            fill
+            className="object-contain hidden dark:block"
+          />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
             src="/reading.png"
             fill
             alt="reading"
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            alt="reading"
+            className="object-contain hidden dark:block"
           />
         </div>
       </div>
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,9 +4,15 @@ import { useScrollTop } from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 import { ModeToggle } from "@/components/ModeToggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div
       className={cn(
@@ -16,6 +22,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get notion-clone free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href={"/documents"}>Enter Notion-Clone</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
